b5b5f00e54646acb0b0ca7728a15cd2debfbf5e1
Daniel Gomez <dannyelgomezz.13@gmail.com>
2022-11-01 17:16:31 -0500

    Clase 4 Completada

--- script.js
+++ script.js
@@ -1,26 +1,46 @@
-const btn = document.querySelector("[data-form-btn]");
-//Fomra mejor
-const createTask = (evento) => {
-    evento.preventDefault();
-    const input = document.querySelector("[data-form-input]");
-    const value = input.value;
-    const list = document.querySelector("[data-list]");
-    const task = document.createElement('li');
-    task.classList.add('card');
-    input.value = '';
-    const content = `<div>
-    <i class="far fa-check-square icon"></i>
-    <span class="task">${value}</span>
-    </div>
+(() => { //evitando que el user manipule codigo
+    const btn = document.querySelector("[data-form-btn]");
+    //Fomra mejor
+    const createTask = (evento) => {
+        evento.preventDefault();
+        const input = document.querySelector("[data-form-input]");
+        const value = input.value;
+        const list = document.querySelector("[data-list]");
+        const task = document.createElement('li');
+        task.classList.add('card');
+        input.value = '';
+        const taskContent = document.createElement("div");
+        taskContent.appendChild(checkComplete());
+        const titleTask = document.createElement("span");
+        titleTask.classList.add("task");
+        titleTask.innerText = value;
+        taskContent.appendChild(titleTask);
+        const content = `
     <i class="fas fa-trash-alt trashIcon icon"></i>`;
-    task.innerHTML = content;
-    list.appendChild(task);
-    console.log(content)
-}
-btn.addEventListener("click", createTask);
-//Fomra normal
-/* btn.addEventListener("click",(evento )=> {
-    evento.preventDefault();
-    const input = document.querySelector("[data-form-input]");
-    console.log(input.value)
-}) */
+        //task.innerHTML = content;
+        task.appendChild(taskContent);
+        list.appendChild(task);
+    }
+    btn.addEventListener("click", createTask);
+    //Fomra normal
+    /* btn.addEventListener("click",(evento )=> {
+        evento.preventDefault();
+        const input = document.querySelector("[data-form-input]");
+        console.log(input.value)
+    }) */
+
+    //<i class="far fa-check-square icon"></i>
+    const checkComplete = () => {
+        const i = document.createElement("i");
+        i.classList.add("far", "fa-check-square", "icon");
+        i.addEventListener("click", completeTask);
+        return i;
+    }
+
+    const completeTask = (event) => {
+        const element = event.target;
+        element.classList.toggle("fas");//agreagando clase nueva del icono
+        element.classList.toggle("completeIcon")
+        element.classList.toggle("far");//eliminado la vieja
+    }
+})();
